ea0e73e315d58e5cdabc1a6dbffbc88c698c5292
Sabina Batrakova <mms8818@mail.ru>
2025-10-19 12:18:23 +0200

    refactor(basic-3.1-3.4, extended-2.1): 3.1 - applied selector naming; 3.2 - applied meaningful naming; 3.3 - applied short classname and id; 3.4 - applied tag selectors; 2.1 - used BEM.

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 //Event handling, user interaction is what starts the code execution.
 
 var taskInput = document.getElementById("new-task");//Add a new task.
-var addButton = document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
+var addButton = document.querySelector(".btn-task__add");//first button
+var incompleteTaskHolder = document.querySelector(".incomplete-tasks");//ul of #incompleteTasks
+var completedTasksHolder = document.querySelector(".completed-tasks");//completed-tasks
 
 //New task list item
 var createNewTaskElement = function(taskString) {
